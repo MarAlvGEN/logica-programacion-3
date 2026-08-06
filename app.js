e0f0d1ce8df479c5832e56907ffb58bd0070820b
Mauricio Alvarez <297007275+MarAlvGEN@users.noreply.github.com>
2026-08-06 00:05:46 -0500

    Better logics

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ const entrada = document.getElementById('entrada');
 const resultado = document.getElementById('resultado');
 form = document.getElementById('formulario');
 
+document.addEventListener('DOMContentLoaded', () => {
+  entrada.value = '5';
+  refresh();
+});
+
 form.addEventListener('input', () => {
+  refresh();
+});
+
+function refresh() {
   let number = calcularFactorial(entrada.value);
 
   resultado.innerHTML = /* html */ `
-<p class="text-danger">${number}></p>
-`;
-});
+<p class="text-danger">${number}</p>
+    `;
+}
 
 function calcularFactorial(num) {
-  let resultado = 1;
+  let init = 1;
 
   for (let i = 1; i <= num; i++) {
-    resultado *= i;
+    init *= i;
   }
 
-  return BigInt(resultado);
+  return BigInt(init);
 }
